5c2a89019e19d0d2cbd6c96a8d8728c9f73a3384
Mauricio Castro <mauriciodesouzaecastro@gmail.com>
2023-06-02 15:48:54 -0300

    fixed query

--- src/repositories/posts.repository.js
+++ src/repositories/posts.repository.js
@@ -6,13 +6,16 @@ class PostRepository {
         COUNT(l.id) AS total_likes, 
                 ARRAY(
                     SELECT unnest(array_agg(u.username)) 
-                    FROM unnest(array_agg(u.username)) 
+                    FROM likes ll
+                    JOIN users u ON ll.user_id = u.id
+                    WHERE ll.post_id = p.id
+                    ORDER BY RANDOM()
                     LIMIT 2
                 ) AS liked_users,
                 EXISTS (
                     SELECT 1 
                     FROM likes 
-                    WHERE user_id = 1 
+                    WHERE user_id = 1
                     AND post_id = p.id
                 ) AS user_liked,
                 (
@@ -21,13 +24,12 @@ class PostRepository {
                     WHERE p.user_id = users.id
                 ) AS author
         FROM posts p
-        JOIN trending_posts tr ON p.id = tr.post_id
-        JOIN trendings t ON t.id = tr.trending_id
+        LEFT JOIN trending_posts tr ON p.id = tr.post_id
         LEFT JOIN likes l ON l.post_id = p.id
         LEFT JOIN users u ON l.user_id = u.id
-        ORDER BY posts.createdAt DESC
-        LIMIT 20
-        GROUP BY p.id, u.username;`;
+        GROUP BY p.id, u.username
+        ORDER BY p.createdat DESC LIMIT 20
+        `;
 
         return db.query(query);
     }
